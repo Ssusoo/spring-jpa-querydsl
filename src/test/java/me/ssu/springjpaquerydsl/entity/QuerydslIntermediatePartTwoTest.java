ec430eaa1ae8b75f020f0176bfb9cec5ae87b407
김수환 <zzanggoon8@hiconsy.com>
2022-03-25 10:20:16 +0900

    bulkuUpate Query ADD

--- src/test/java/me/ssu/springjpaquerydsl/entity/QuerydslIntermediatePartTwoTest.java
+++ src/test/java/me/ssu/springjpaquerydsl/entity/QuerydslIntermediatePartTwoTest.java
@@ -1,18 +1,17 @@
 package me.ssu.springjpaquerydsl.entity;
 
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import me.ssu.springjpaquerydsl.common.BaseTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Commit;
 
 import java.util.List;
 
 import static me.ssu.springjpaquerydsl.entity.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.map;
 
 public class QuerydslIntermediatePartTwoTest extends BaseTest {
 
@@ -121,5 +120,53 @@ public class QuerydslIntermediatePartTwoTest extends BaseTest {
     // TODO 위에 두 개를 조합해서 쓸 때는 위에 둘을 Predicate -> BooleanExpression로 수정하기
     private BooleanExpression allEq(String usernameCond, Integer ageCond) {
         return usernameEq(usernameCond).and(ageEq(ageCond));
+    }
+
+    /**
+     * 수정, 삭제 벌크 연산
+     *  멤버의 나이가 28살 미만이면 비회원으로 이름을 바꾼다.
+     *
+     * 벌크 연산의 단점
+     *  영속성 컨텍스트의 DB 상태와 벌크 연산의 DB 상태가 다름
+     *  벌크 연산은 영속성 컨텍스트를 무시한 채 DB의 데이터를 바꿔버림림
+     */
+    // TODO @Transaction 시키기면 Rollback하기 때문에
+    @Test
+    @Commit
+    void bulkUpdate() {
+        // TODO 벌크 연산 실행되기 전
+        //  member1 = 10 -> DB member1
+        //  member2 = 20 -> DB member1
+        //  member3 = 30 -> DB member3
+        //  member4 = 40 -> DB member4
+
+        // TODO count는 영향을 받은 row 수가 나옴.
+        long count = queryFactory
+                .update(member)
+                .set(member.username, "비회원")
+                .where(member.age.lt(28))   // lt age < 28
+                .execute();
+
+        // TODO 벌크 연산 실행 후(영속성 컨텍스트를 무시한 채 DB를 바꿔버림)
+        //  id=1 member1 = 10 -> id=1 DB 비회원
+        //  id=2 member2 = 20 -> id=2 DB 비회원
+        //  id=3 member3 = 30 -> id=3 DB member3
+        //  id=4 member4 = 40 -> id=4 DB member4
+
+        // TODO JPA 영속성 컨텍스트와 벌크연산 해결 방법
+        entityManager.flush(); // 영속성 컨텍스트의 변경 내용을 DB에 저장
+        entityManager.clear(); // 영속성 컨텍스트의 값을 초기화
+
+        // TODO SQL이 실행되고 DB에서는 값을 가져온다.
+        //  JPA DB의 결과를 영속성 컨텍스트에 넣어 줘야하는데
+        //  영속성 컨텍스트에 member1의 중복이 있으면 DB의 값을 버리고
+        //  순위는 영속성 컨텍스트가 우선권을 가짐.
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        for (Member member1 : result) {
+            System.out.println("member1 = " + member1);
+        }
     }
 }
