d696733c86a9b7feb26226f102006cb5cc14ec9b
김수환 <zzanggoon8@hiconsy.com>
2022-03-25 20:12:15 +0900

    순수JPA -> QueryDSL Query Modified

--- src/main/java/me/ssu/springjpaquerydsl/repository/MemberJpaRepository.java
+++ src/main/java/me/ssu/springjpaquerydsl/repository/MemberJpaRepository.java
@@ -2,11 +2,14 @@ package me.ssu.springjpaquerydsl.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import me.ssu.springjpaquerydsl.entity.Member;
+import me.ssu.springjpaquerydsl.entity.QMember;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
+
+import static me.ssu.springjpaquerydsl.entity.QMember.*;
 
 // TODO DAO와 같은 개념(Entity 조회하기 위한 어떤 계층)
 @Repository
@@ -34,16 +37,31 @@ public class MemberJpaRepository {
         return Optional.ofNullable(findMember);
     }
 
-    // TODO 전체 조회
+    // TODO 전체 조회(순수 JPA)
     public List<Member> findAll() {
         return entityManager.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
 
-    // TODO username으로 조회
+    // TODO 전체 조회(QueryDSL)
+    public List<Member> findAll_Querydsl() {
+        return queryFactory
+                // TODO QMember.member하고 static import 해서 member로 바꾸기
+                .selectFrom(member)
+                .fetch();
+    }
+
+    // TODO username으로 조회(순수 JPA)
     public List<Member> findByUsername(String username) {
         return entityManager.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", username)
                 .getResultList();
+    }
+    // TODO username으로 조회(QueryDSL)
+    public List<Member> findByUsername_Querydsl(String username) {
+        return queryFactory
+                .selectFrom(member)
+                .where(member.username.eq(username))
+                .fetch();
     }
 }
