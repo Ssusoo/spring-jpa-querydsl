b40b2a6d1a7bafcb45b36a08bfbf2037c3dc2bb6
김수환 <zzanggoon8@hiconsy.com>
2022-03-25 21:39:20 +0900

    MemberSearchCondition Data

--- src/main/java/me/ssu/springjpaquerydsl/dto/MemberSearchCondition.java
+++ src/main/java/me/ssu/springjpaquerydsl/dto/MemberSearchCondition.java
@@ -2,6 +2,10 @@ package me.ssu.springjpaquerydsl.dto;
 
 import lombok.Data;
 
+/* TODO @Data
+    Getter, Setter, RequiredArgsConstructor,
+    ToString, EqualsAndHashCode
+*/
 @Data
 public class MemberSearchCondition {
     // TODO 회원명, 팀명, 나이(ageGoe >= , ageLoe <=)
--- src/test/java/me/ssu/springjpaquerydsl/repository/MemberJpaRepositoryPartOneTest.java
+++ src/test/java/me/ssu/springjpaquerydsl/repository/MemberJpaRepositoryPartOneTest.java
@@ -1,7 +1,9 @@
 package me.ssu.springjpaquerydsl.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import me.ssu.springjpaquerydsl.common.JpaBaseTest;
 import me.ssu.springjpaquerydsl.entity.Member;
+import me.ssu.springjpaquerydsl.entity.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +11,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MemberJpaRepositoryTest extends JpaBaseTest {
+class MemberJpaRepositoryPartOneTest extends JpaBaseTest {
 
     /**
      * 순수 JPA Test
@@ -76,5 +78,33 @@ class MemberJpaRepositoryTest extends JpaBaseTest {
         //  findByUsername_Querydsl(String username) == member1
         List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
         assertThat(result2).containsExactly(member);
+    }
+    /**
+     *
+     */
+    @Test
+    void searchTest() {
+        // TODO Team 객체 생성
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        // TODO DB에 넣기
+        entityManager.persist(teamA);
+        entityManager.persist(teamB);
+
+        // TODO Member 객체 생성
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        // TODO DB에 넣기
+        entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.persist(member3);
+        entityManager.persist(member4);
+
+
     }
 }
