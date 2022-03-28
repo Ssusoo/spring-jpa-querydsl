56b8e836b07068ef19562a419c58e6b454632149
zzanggoon8 <zzanggoon8@gmail.com>
2022-03-29 00:31:03 +0900

    사용자 정의 인터페이스 상속

--- src/main/java/me/ssu/springjpaquerydsl/repository/MemberSpringJpaRepository.java
+++ src/main/java/me/ssu/springjpaquerydsl/repository/MemberSpringJpaRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 // TODO 스프링 데이터 JPA(Interface끼리 상속받음)
-// TODO 스프링 데이터 JPA + QueryDSL 사용하기
+// TODO 스프링 데이터 리포지토리에 사용자 정의 인터페이스 상속
+//  스프링 데이터 JPA + QueryDSL 사용하기
 //  인터페이스는 여러 개를 상속받을 수 있음
 public interface MemberSpringJpaRepository extends JpaRepository<Member, Long>, MemberSpringJpaRepositoryCustom {
     // TODO findByUsername(select m from Member m where m.username=?)
