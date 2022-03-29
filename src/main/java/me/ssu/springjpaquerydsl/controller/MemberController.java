88e95858578754c703b7e16ded07b84b6ccb37f0
zzanggoon8 <zzanggoon8@gmail.com>
2022-03-29 22:10:14 +0900

    Paging Controller

--- src/main/java/me/ssu/springjpaquerydsl/controller/MemberController.java
+++ src/main/java/me/ssu/springjpaquerydsl/controller/MemberController.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import me.ssu.springjpaquerydsl.dto.MemberSearchCondition;
 import me.ssu.springjpaquerydsl.dto.MemberTeamDto;
 import me.ssu.springjpaquerydsl.repository.MemberRealJpaRepository;
+import me.ssu.springjpaquerydsl.repository.MemberSpringJpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +17,25 @@ import java.util.List;
 public class MemberController {
 
     private final MemberRealJpaRepository memberJpaRepository;
+    private final MemberSpringJpaRepository memberSpringJpaRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         // TODO Where절에 파라미터를 사용한 예제
         return memberJpaRepository.search(condition);
+    }
+
+    // TODO 스프링 데이터 JPA에서 제공하는 페이징 처리
+    //  전체 카운트를 한번에 조회하는 단순한 방법
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberSpringJpaRepository.searchPageSimple(condition, pageable);
+    }
+
+    // TODO 스프링 데이터 JPA에서 제공하는 페이징 처리
+    //  데이터 내용과 전체 카운트를 별도로 조회하는 방법
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberSpringJpaRepository.searchPageComplex(condition, pageable);
     }
 }
