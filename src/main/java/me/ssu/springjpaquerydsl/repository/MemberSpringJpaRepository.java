2ce8286d71b5e93f33bfbe6bbb8c4e14e890799b
김수환 <zzanggoon8@hiconsy.com>
2022-03-30 09:18:52 +0900

    스프링 데이터 JPA Repository

--- src/main/java/me/ssu/springjpaquerydsl/repository/MemberSpringJpaRepository.java
+++ src/main/java/me/ssu/springjpaquerydsl/repository/MemberSpringJpaRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * 스프링 데이터 JPA임.
+ */
 // TODO 스프링 데이터 JPA(Interface끼리 상속받음)
 // TODO 스프링 데이터 리포지토리에 사용자 정의 인터페이스 상속
 //  스프링 데이터 JPA + QueryDSL 사용하기
