470889904122c043c5619a2f897a796c3f8643a8
김수환 <zzanggoon8@hiconsy.com>
2022-04-01 20:24:51 +0900

    스프링 데이터 JPA에서 제공하는 QueryDSL-3(QuerydslPredicateExecutor<T> ADD

--- src/main/java/me/ssu/springjpaquerydsl/repository/MemberSpringJpaRepository.java
+++ src/main/java/me/ssu/springjpaquerydsl/repository/MemberSpringJpaRepository.java
@@ -3,17 +3,19 @@ package me.ssu.springjpaquerydsl.repository;
 
 import me.ssu.springjpaquerydsl.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
 /**
  * 스프링 데이터 JPA임.
  */
-// TODO 스프링 데이터 JPA(Interface끼리 상속받음)
-// TODO 스프링 데이터 리포지토리에 사용자 정의 인터페이스 상속
+// TODO 스프링 데이터 JPA(Interface끼리 상속받음)-1
+// TODO 스프링 데이터 리포지토리에 사용자 정의 인터페이스 상속-2
 //  스프링 데이터 JPA + QueryDSL 사용하기
 //  인터페이스는 여러 개를 상속받을 수 있음
-public interface MemberSpringJpaRepository extends JpaRepository<Member, Long>, MemberSpringJpaRepositoryCustom {
+// TODO 스프링 데이터 JPA에서 제공하는 QueryDSL-3(QuerydslPredicateExecutor<T>
+public interface MemberSpringJpaRepository extends JpaRepository<Member, Long>, MemberSpringJpaRepositoryCustom, QuerydslPredicateExecutor<Member> {
     // TODO findByUsername(select m from Member m where m.username=?)
     //  기존 순수 JPA(MemberJpaRepository)에서 만든 대부분 코드가 사라짐(findByUsername은 만들어주어야 함)
     List<Member> findByUsername(String username);
---
+++ src/test/java/me/ssu/springjpaquerydsl/repository/MemberSpringJpaQuerydslPredicateExecutorRepositoryTest.java
@@ -0,0 +1,43 @@
+package me.ssu.springjpaquerydsl.repository;
+
+
+import me.ssu.springjpaquerydsl.common.JpaBaseTest;
+import me.ssu.springjpaquerydsl.entity.Member;
+import me.ssu.springjpaquerydsl.entity.QMember;
+import me.ssu.springjpaquerydsl.entity.Team;
+import org.junit.jupiter.api.Test;
+
+public class MemberSpringJpaQuerydslPredicateExecutorRepositoryTest extends JpaBaseTest {
+
+    @Test
+    void querydslPredicateExecutorTest() {
+        // TODO Team 객체 생성
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        // TODO DB에 넣기
+        entityManager.persist(teamA);
+        entityManager.persist(teamB);
+
+        // TODO Member 객체 생성
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        // TODO DB에 넣기
+        entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.persist(member3);
+        entityManager.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberSpringJpaRepository.findAll(
+                member.age.between(20, 40)
+                        .and(member.username.eq("member11")));
+        for (Member findMember : result) {
+            System.out.println("member11" + findMember);
+        }
+    }
+}
