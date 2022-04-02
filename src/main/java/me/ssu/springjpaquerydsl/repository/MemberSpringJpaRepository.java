2c49094d3ad6313e6767d17446d5d9b7de0d3df4
zzanggoon8 <zzanggoon8@gmail.com>
2022-04-02 23:53:35 +0900

    스프링 데이터 JPA에서 제공하는 QueryDSL-3 etc

--- src/main/java/me/ssu/springjpaquerydsl/repository/MemberSpringJpaRepository.java
+++ src/main/java/me/ssu/springjpaquerydsl/repository/MemberSpringJpaRepository.java
@@ -10,8 +10,8 @@ import java.util.List;
 /**
  * 스프링 데이터 JPA임.
  */
-// TODO 스프링 데이터 JPA(Interface끼리 상속받음)-1
-// TODO 스프링 데이터 리포지토리에 사용자 정의 인터페이스 상속-2
+// TODO 스프링 데이터 JPA(Interface끼리 상속받음)--1
+// TODO 스프링 데이터 리포지토리에 사용자 정의 인터페이스 상속--2
 //  스프링 데이터 JPA + QueryDSL 사용하기
 //  인터페이스는 여러 개를 상속받을 수 있음
 // TODO 스프링 데이터 JPA에서 제공하는 QueryDSL-3(QuerydslPredicateExecutor<T>
